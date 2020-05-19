fba6c0ed80a484192b96ebaf163255abe6eb6824
Danielle Spencer <d.m.spencer.0486@gmail.com>
2020-05-18 21:58:40 -0500

    updated random gen background image on mobile

--- src/components/layout/RandomGenerator.js
+++ src/components/layout/RandomGenerator.js
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom';
 
 const RandomGenerator = () => {
     return (
-        <section className="randomGen">
-            <div>
+       
+            <div className="randomGen">
                 <div className="randomGen-inner">
                 <h1 className="x-large">STFU & EAT</h1>
                 <p className="lead">
-                    We have all been there. With a group of friends, starving, but no one can make a decision. Should we get Thai? or pizza?
+                <span className='hide-sm'> We have all been there. With a group of friends, starving, but no one can make a decision. Should we get Thai? or pizza?
 
                     STFU&EAT acts as your trigger man, providing a random restaurant recommendation for those hangry moments. All local St. Louis CITY restaurants that are under $25 for one person, chains excluded.
 
-                    The best part? Great food and no obligation to go to the place suggested by your friend of a friend of a friend. You get the idea. Hit the "Let's Eat!" button and your culinary journey begins
+                    The best part? Great food and no obligation to go to the place suggested by your friend of a friend of a friend. You get the idea. </span> Hit the "Let's Eat!" button and your culinary journey begins
                 </p>
                 <div className="buttons">
                     <Link to="/recommendation" className="btn btn-success btn-lg">
@@ -24,7 +24,7 @@ const RandomGenerator = () => {
                 </div>
                 </div>
             </div>
-        </section>
+     
     )
 }
 
