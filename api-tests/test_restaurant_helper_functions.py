12c42e4a5d086da24a5800d2fe5a02c31a116354
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-05-26 18:57:54 -0500

    simplified file structure, trying to get pytest to work without changing import statements

--- api-tests/test_restaurant_helper_functions.py
+++ api-tests/test_restaurant_helper_functions.py
@@ -1,8 +1,8 @@
 import pytest
 
-from api.main.restaurant_helper_functions import organize_restaurant_output, select_random_restaurant
-from api.main.routes import app
-from api.main.db import restaurants
+from api.restaurant_helper_functions import organize_restaurant_output, select_random_restaurant
+from api.routes import app
+from api.db import restaurants
 
 def test_organize_restaurant_output_returns_expected_keys():
     with app.app_context():
