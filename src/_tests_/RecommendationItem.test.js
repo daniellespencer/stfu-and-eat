96e4c52472cadb2e1d48f17bb0c420e0aa5b41de
Danielle Spencer <d.m.spencer.0486@gmail.com>
2020-06-05 14:46:55 -0500

    updated RecommendationItem test

--- src/_tests_/RecommendationItem.test.js
+++ src/_tests_/RecommendationItem.test.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import { render, cleanup, fireEvent } from '@testing-library/react';
-import { shallow } from 'enzyme';
+import renderer, { act } from 'react-test-renderer';
+import { mount } from 'enzyme';
 import ReactDOM from 'react-dom';
 
 import RecommendationItem from '../components/layout/RecommendationItem';
+export const flushPromises = () => new Promise(setImmediate)
+const restaurant = {
+    "address": "2901 Salena St, St. Louis, MO 63118",
+    "cuisine": "American",
+    "id": "5ec1878e67e5bfad7604154f",
+    "name": "Benton Park Cafe",
+    "neighborhood": "Benton Park",
+    "website": "http://www.bentonparkcafe.com/"
+  }
 
-afterEach(cleanup);
+describe("RecommendationItem", () => {
+    beforeEach(() => {jest.useFakeTimers()})
+    afterEach(() => {
+        jest.clearAllTimers()
+        cleanup()
+    });
+    // Set up
+    
+    // execution
 
-it("renders correctly", () => {
-    const restaurant = jest.fn();
-    shallow(<RecommendationItem restaurant={restaurant} />);
-})
+    // assertion
+    it("renders restaurant recommendation", async () => {
+        fetch.mockResponse(JSON.stringify({result: [
+         restaurant
+        ]}))
+        let restaurantRecommendation
+
+        await act( async () => {
+            restaurantRecommendation = renderer.create(<RecommendationItem restaurant={restaurant} />)
+            await flushPromises()
+        });
+        
+
+        expect(restaurantRecommendation.toJSON()).toMatchSnapshot()
+    });
+})    
